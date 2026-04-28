434cc88d9fd0db566bb9f37f2bb95bd26f612887
Suveen Kumar <suveenkumar1997@gmail.com>
2026-04-28 18:09:14 +0530

    UI Updates

--- script.js
+++ script.js
@@ -1,20 +1,68 @@
+const promptInput = document.getElementById("promptInput");
+const warning = document.getElementById("wordWarning");
+const videoLength = document.getElementById("videoLength");
+const videoQuality = document.getElementById("videoQuality");
+const videoType = document.getElementById("videoType");
+const videoAIModel = document.getElementById("videoAIModel");
 
-const fileInput = document.getElementById("fileInput");
-const previewImg = document.getElementById("previewImg");
-const plusIcon = document.getElementById("plusIcon");
 
-fileInput.addEventListener("change", function () {
-    const file = this.files[0];
+const MAX_CHARACTERS = 1000;
 
-    if (file) {
-        const reader = new FileReader();
-
-        reader.onload = function (e) {
-            previewImg.src = e.target.result;
-            previewImg.classList.remove("hidden");
-            plusIcon.style.display = "none";
-        };
-
-        reader.readAsDataURL(file);
-    }
+promptInput.addEventListener("input", function () {
+    let words = this.value;
+    let wordCount = words.length;
+    handleValidation(wordCount);
+    Save();
 });
+
+videoLength.addEventListener("change", function () {
+    Save();
+});
+videoQuality.addEventListener("change", function () {
+    Save();
+});
+videoType.addEventListener("change", function () {
+    Save();
+});
+videoAIModel.addEventListener("change", function () {
+    Save();
+});
+
+// Restore on load
+window.addEventListener("DOMContentLoaded", function () {
+    Load();
+
+});
+
+function Load() {
+    promptInput.value = localStorage.getItem("promptInput");
+    handleValidation(promptInput.value.length);
+
+    videoLength.value = localStorage.getItem("videoLength");;
+    videoQuality.value = localStorage.getItem("videoQuality");;
+    videoType.value = localStorage.getItem("videoType");;
+    videoAIModel.value = localStorage.getItem("videoAIModel");;
+}
+function handleValidation(wordCount) {
+    if (wordCount > MAX_CHARACTERS) {
+        warning.textContent = `${wordCount} / ${MAX_CHARACTERS}`;
+
+        warning.style.background = "rgba(255,0,0,0.2)";
+        warning.style.color = "#ff4d4f";
+        generateBtn.disabled = true;
+    } else {
+        warning.textContent = `${wordCount} / ${MAX_CHARACTERS}`;
+        warning.style.background = "rgba(255,255,255,0.1)";
+        warning.style.color = "#aaa";
+        generateBtn.disabled = false;
+
+    }
+}
+
+function Save() {
+    localStorage.setItem("promptInput", promptInput.value);
+    localStorage.setItem("videoLength", videoLength.value);
+    localStorage.setItem("videoQuality", videoQuality.value);
+    localStorage.setItem("videoType", videoType.value);
+    localStorage.setItem("videoAIModel", videoAIModel.value);
+}
